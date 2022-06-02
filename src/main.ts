78fc23099bf84494bcd04da4d15323fec81049f7
tangying <tangying@ganguo.hk>
2022-06-02 14:14:41 +0800

    拦截器

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('/api')
+    app.setGlobalPrefix('/api');
+    app.useGlobalInterceptors(new TransformInterceptor());
+    const options = new DocumentBuilder()
+        .setTitle('Blog Api')
+        .setDescription('The Blog Api Description')
+        .setVersion('1.0')
+        .build();
 
-   const options = new DocumentBuilder()
-      .setTitle('Blog Api')
-      .setDescription('The Blog Api Description')
-      .setVersion('1.0')
-      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('doc', app, document);
 
-   const document = SwaggerModule.createDocument(app, options);
-   SwaggerModule.setup('doc', app, document);
-
-   await app.listen(3005);
-   console.log('app running in 3005');
+    await app.listen(3005);
+    console.log('app running in 3005');
 }
 bootstrap();
